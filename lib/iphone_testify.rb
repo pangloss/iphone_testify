3044f670e02f4f8df3fab3a447161dc613df1c3e
Paul Wilson <paul.wilson@merecomplexities.com>
2009-02-25 16:21:14 +0000

    bumped version

--- lib/iphone_testify.rb
+++ lib/iphone_testify.rb
@@ -2,7 +2,7 @@
 module IphoneTestify
   GEMDIR = File.expand_path(File.dirname(__FILE__) + "/..")
   # :stopdoc:
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
